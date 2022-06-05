0bcd6dd951a2944843f3d03bc01e5fbacbcddcf6
juniq <juniqlim@gmail.com>
2022-06-05 20:28:39 +0900

    init

---
+++ src/main/kotlin/im/juniq/kotlinboard/KotlinBoardApplication.kt
@@ -0,0 +1,11 @@
+package im.juniq.kotlinboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinBoardApplication>(*args)
+}
---
+++ src/test/kotlin/im/juniq/kotlinboard/KotlinBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package im.juniq.kotlinboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
